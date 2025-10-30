6955ea7b4d693e08172dce491259e357771867ee
Phoebe Jao <phoebe_jao@commude.ph>
2025-10-30 09:55:02 +0800

    [fix] top page about section

--- src/components/top/AboutSection.jsx
+++ src/components/top/AboutSection.jsx
@@ -47,10 +47,8 @@ const AboutSection = () => {
             <div className="aboutSec__content">
               <div className="aboutSec__nameCont">
                 <p className="aboutSec__bigTxt">Phoebe</p>
-                <div className="aboutSec__imgCont">
-                  <div className="aboutSec__imgWrap">
-                    <img src={img} alt="Phoebe Jao" className="aboutSec__img" />
-                  </div>
+                <div className="aboutSec__imgWrap">
+                  <img src={img} alt="Phoebe Jao" className="aboutSec__img" />
                 </div>
               </div>
               <p className="aboutSec__smallTxt">I am a Junior Frontend Web Developer based in the Philippines, born in 2000. I am passionate about building clean, responsive websites and constantly improving my skills in web development.</p>
